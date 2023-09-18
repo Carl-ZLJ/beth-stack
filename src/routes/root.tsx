73e8f1a45d1ef413d14ec28a9ac3826362fed33a
kaelzhu <zljmms32@gmail.com>
2023-09-18 19:33:08 +0800

    chore: bump elysia and html

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,17 +1,16 @@
-import { Fragment, html } from '@elysiajs/html'
+import { html } from '@elysiajs/html'
 import { BaseHTML } from '../components/BaseHTML'
-import Elysia from 'elysia'
+import { Elysia } from 'elysia'
+
 
 export const rootRoute = new Elysia()
-    // @ts-ignore
     .use(html())
-    // @ts-ignore
     .get('/', ({ html }) => html(
         <BaseHTML>
-            <Fragment>
+            <>
                 <button hx-get='/clicked' hx-swap='outerHTML'>Click me</button>
                 <div hx-get='/todos' hx-trigger='load' hx-swap='innerHTML'></div>
-            </Fragment>
+            </>
         </BaseHTML>
     ))
     .get('/clicked', () => 'Clicked!')
